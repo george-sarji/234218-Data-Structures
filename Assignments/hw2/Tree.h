ae2d6f5c950bd76aeca34e88174a2ec8946403fb
george-sarji <georgesarji@gmail.com>
2021-05-13 20:32:57 +0300

    Added documentation and copied tree files to folder

---
+++ Assignments/hw2/Tree.h
@@ -0,0 +1,52 @@
+#ifndef TREE_H_
+#define TREE_H_
+
+namespace structures
+{
+    template <class T>
+    class Tree
+    {
+    private:
+        T data;
+        Tree *left;
+        Tree *right;
+        int height;
+
+    public:
+        Tree() = default;
+        Tree(T data, Tree *left = nullptr, Tree *right = nullptr) : data(data), left(left), right(right), height(1) {}
+        Tree(const Tree &tree);
+        ~Tree() = default;
+
+        // Operator overloads
+        T &operator*();
+        Tree &operator=(const Tree &tree);
+        bool operator>(const Tree &tree) const;
+        bool operator>=(const Tree &tree) const;
+        bool operator<(const Tree &tree) const;
+        bool operator<=(const Tree &tree) const;
+        bool operator==(const Tree &tree) const;
+
+        // Utility methods
+        Tree *Left() const;
+        Tree *Right() const;
+        int Height();
+        int getBalanceFactor() const;
+        Tree *LeftLeft(Tree *root);
+        Tree *LeftRight(Tree *root);
+        Tree *RightLeft(Tree *root);
+        Tree *RightRight(Tree *root);
+        void printInOrder() const;
+        void printPreOrder() const;
+
+        // Addition/removal functions
+        Tree *addIntersection(T data);
+        Tree *removeIntersection(T data);
+        Tree *inorderSuccessor();
+
+        // ! Required functions
+        // LeftRight, RightLeft
+    };
+}
+
+#endif
