ac05756b6e2f3f1dddd94b456ec68c0acbb60506
george-sarji <georgesarji@gmail.com>
2021-05-25 15:41:33 +0300

    Added error handling for bad allocations

--- Assignments/hw2/CarDealershipManager.cpp
+++ Assignments/hw2/CarDealershipManager.cpp
@@ -73,7 +73,6 @@ namespace structures
         try
         {
             // Add the type to the types tree.
-            // TODO: Fix the bug with the data update in addIntersection on the second add.
             this->types = this->types->addIntersection(newType);
         }
         catch (const structures::TreeException &error)
@@ -146,6 +145,10 @@ namespace structures
                 throw FailureError();
             }
         }
+        catch (const std::bad_alloc &e)
+        {
+            throw MemoryError();
+        }
         if (vertex == nullptr)
         {
             throw FailureError();
@@ -178,9 +181,18 @@ namespace structures
             Tree<SalesNode> *temp_model = this->car_sales->getLargest();
             if (temp_model)
             {
+                CarModel *old_best = this->bestModel;
                 // We have a good best seller.
-                delete this->bestModel;
-                this->bestModel = new CarModel(*temp_model->Data()->getModel());
+                try
+                {
+                    this->bestModel = new CarModel(*temp_model->Data()->getModel());
+                }
+                catch (const std::bad_alloc &e)
+                {
+                    delete temp;
+                    throw MemoryError();
+                }
+                delete old_best;
             }
             else
             {
@@ -271,7 +283,15 @@ namespace structures
         }
         // We need to search for the model. Check if the sales of the model are 0.
         CarModel *requested_model = requested_node->Data()->Models()[modelID];
-        CarModel *old_model = new CarModel(*requested_model);
+        CarModel *old_model = nullptr;
+        try
+        {
+            old_model = new CarModel(*requested_model);
+        }
+        catch (const std::bad_alloc &e)
+        {
+            throw MemoryError();
+        }
         Tree<CarModel> *new_sold_node = nullptr;
         bool deleted_smallest = false;
         // We still have to update the data.
@@ -279,13 +299,21 @@ namespace structures
         requested_model->Grade() += 10;
         if (requested_model->Sales() == 1)
         {
-            //     // We need to search in the no sales tree.
+            // We need to search in the no sales tree.
             TypeNode requested_type(typeID);
             Tree<TypeNode> *sales_node = this->non_sold_models->findData(requested_type);
             // We are supposed to get a valid node.
             // We should have the model in here.
             // We have to add the new car model into the sales tree with a NEW variable
-            sold_models = sold_models->addIntersection(new CarModel(*requested_model));
+            try
+            {
+                sold_models = sold_models->addIntersection(new CarModel(*requested_model));
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                throw MemoryError();
+            }
             new_sold_node = sold_models->findData(*requested_model);
             // We need to remove it from the given tree.
             // We have to update the car_sales tree.
@@ -297,10 +325,26 @@ namespace structures
             // Check if we have no car sales now.
             if (this->car_sales == nullptr)
             {
-                this->car_sales = new Tree<SalesNode>();
+                try
+                {
+                    this->car_sales = new Tree<SalesNode>();
+                }
+                catch (const std::bad_alloc &e)
+                {
+                    delete old_model;
+                    throw MemoryError();
+                }
             }
             // Add the new model.
-            this->car_sales = this->car_sales->addIntersection(new SalesNode(new CarModel(*requested_model)));
+            try
+            {
+                this->car_sales = this->car_sales->addIntersection(new SalesNode(new CarModel(*requested_model)));
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                throw MemoryError();
+            }
             // We can now finally remove the requested model:
             Tree<CarModel> *models = sales_node->Data()->getModels();
             models = models->removeIntersection(old_model);
@@ -332,7 +376,15 @@ namespace structures
                 // Check if we have a null tree.
                 if (this->non_sold_models == nullptr)
                 {
-                    this->non_sold_models = new Tree<TypeNode>();
+                    try
+                    {
+                        this->non_sold_models = new Tree<TypeNode>();
+                    }
+                    catch (const std::bad_alloc &e)
+                    {
+                        delete old_model;
+                        throw MemoryError();
+                    }
                 }
             }
             else
@@ -356,17 +408,56 @@ namespace structures
             this->sold_models = this->sold_models->removeIntersection(old_model);
             if (this->sold_models == nullptr)
             {
-                this->sold_models = new Tree<CarModel>();
+                try
+                {
+                    this->sold_models = new Tree<CarModel>();
+                }
+                catch (const std::bad_alloc &e)
+                {
+                    delete old_model;
+                    throw MemoryError();
+                }
             }
-            this->sold_models = this->sold_models->addIntersection(new CarModel(*requested_model));
+            try
+            {
+                this->sold_models = this->sold_models->addIntersection(new CarModel(*requested_model));
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                throw MemoryError();
+            }
             new_sold_node = this->sold_models->findData(*requested_model);
             // We have to update the car_sales tree.
             // We need to remove the old sales node and add the new one.
-            SalesNode *new_node = new SalesNode(new CarModel(*requested_model)), *to_remove = new SalesNode(new CarModel(*old_model));
+            SalesNode *new_node = nullptr, *to_remove = nullptr;
+            try
+            {
+                new_node = new SalesNode(new CarModel(*requested_model));
+                to_remove = new SalesNode(new CarModel(*old_model));
+            }
+            catch (const std::bad_alloc &e)
+            {
+                if (new_node != nullptr)
+                    delete new_node;
+                if (to_remove != nullptr)
+                    delete to_remove;
+                throw MemoryError();
+            }
             this->car_sales = this->car_sales->removeIntersection(to_remove);
             if (this->car_sales == nullptr)
             {
-                this->car_sales = new Tree<SalesNode>();
+                try
+                {
+                    this->car_sales = new Tree<SalesNode>();
+                }
+                catch (const std::bad_alloc &e)
+                {
+                    delete new_node;
+                    delete to_remove;
+                    delete old_model;
+                    throw MemoryError();
+                }
             }
             this->car_sales = this->car_sales->addIntersection(new_node);
             delete to_remove;
@@ -385,13 +476,29 @@ namespace structures
         // Let's check the best seller now.
         if (this->bestModel == nullptr)
         {
-            this->bestModel = new CarModel(*requested_model);
+            try
+            {
+                this->bestModel = new CarModel(*requested_model);
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                throw MemoryError();
+            }
         }
         else if (requested_model->isBetterSeller(*this->bestModel))
         {
             // Update the best model.
             delete this->bestModel;
-            this->bestModel = new CarModel(*requested_model);
+            try
+            {
+                this->bestModel = new CarModel(*requested_model);
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                throw MemoryError();
+            }
         }
         // Update the type-specific best seller.
         if (requested_model->isBetterSeller(requested_node->Data()->bestSeller()))
@@ -410,7 +517,15 @@ namespace structures
             throw InvalidInput();
         }
         // We need to get the type.
-        CarType *temp_type = new CarType(typeID);
+        CarType *temp_type = nullptr;
+        try
+        {
+            temp_type = new CarType(typeID);
+        }
+        catch (const std::bad_alloc &e)
+        {
+            throw MemoryError();
+        }
         Tree<CarType> *current_type = nullptr;
         try
         {
@@ -418,22 +533,34 @@ namespace structures
         }
         catch (const DoesntExist &e)
         {
+            delete temp_type;
             throw FailureError();
         }
         // Did we find the given type?
         if (current_type == nullptr)
         {
+            delete temp_type;
             throw FailureError();
         }
         // We have the type in the tree. Let's check for the model.
         if (current_type->Data()->numOfModels() <= modelID)
         {
             // No proper model.
+            delete temp_type;
             throw FailureError();
         }
         // We have the model, confirmed. Fetch it.
         CarModel *current_model = current_type->Data()->Models()[modelID];
-        CarModel *old_model = new CarModel(*current_model);
+        CarModel *old_model = nullptr;
+        try
+        {
+            old_model = new CarModel(*current_model);
+        }
+        catch (const std::bad_alloc &e)
+        {
+            delete temp_type;
+            throw MemoryError();
+        }
         // We should have sold it earlier, according to the documentation. That means we check in the sold_cars and the car_sales.
         current_model->Grade() -= 100 / t;
         current_model->Complaints()++;
@@ -441,9 +568,27 @@ namespace structures
         this->sold_models = this->sold_models->removeIntersection(old_model);
         if (this->sold_models == nullptr)
         {
-            this->sold_models = new Tree<CarModel>();
+            try
+            {
+                this->sold_models = new Tree<CarModel>();
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete old_model;
+                delete temp_type;
+                throw MemoryError();
+            }
         }
-        this->sold_models = this->sold_models->addIntersection(new CarModel(*current_model));
+        try
+        {
+            this->sold_models = this->sold_models->addIntersection(new CarModel(*current_model));
+        }
+        catch (const std::bad_alloc &e)
+        {
+            delete old_model;
+            delete temp_type;
+            throw MemoryError();
+        }
         this->smallest_sold_model = this->sold_models->getSmallest();
         delete old_model;
         delete temp_type;
@@ -590,87 +735,3 @@ namespace structures
     }
 
 }
-
-//int main()
-//{
-//    structures::CarDealershipManager *m = new structures::CarDealershipManager();
-//    m->AddCarType(1, 1);
-//    m->AddCarType(2, 1);
-//    m->AddCarType(3, 5);
-//    m->SellCar(1, 0);
-//    m->Quit();
-//    delete m;
-//    return 0;
-//}
-
-//  // Check if we have to use the zero grade.
-//         Tree<TypeNode> *prev_node;
-//         while ((non_sold_models != nullptr || sold_models != nullptr) && sold_models->Data()->Grade() <= 0 && sold_models->Left()->Data()->Grade() >= 0 && counter != threshhold)
-//         {
-//             if(non_sold_models != nullptr)
-//             // Use the non sold models, as they have a grade of zero.
-//             // Do we have a left branch that has not been gone through?
-//             if (non_sold_models->Left() != nullptr && prev_node != non_sold_models->Left())
-//             {
-//                 prev_node = non_sold_models;
-//                 non_sold_models = non_sold_models->Left();
-//                 continue;
-//             }
-//             // Add the current.
-//             Tree<CarModel> *models = non_sold_models->Data()->getSmallestModel();
-//             Tree<CarModel> *prev;
-//             while (models != nullptr && counter != threshhold)
-//             {
-//                 // Check the left.
-//                 if (models->Left() != nullptr && prev != models->Left())
-//                 {
-//                     prev = models;
-//                     models = models->Left();
-//                     continue;
-//                 }
-//                 // Add the current type.
-//                 type_array[counter] = models->Data()->Type();
-//                 model_array[counter] = models->Data()->Id();
-//                 counter++;
-//                 // Check the right.
-//                 if (models->Right() != nullptr && prev != models->Right())
-//                 {
-//                     prev = models;
-//                     models = models->Right();
-//                     continue;
-//                 }
-//                 prev = models;
-//                 models = models->Parent();
-//             }
-//             // Check the right.
-//             if (non_sold_models->Right() != nullptr && prev_node != non_sold_models->Right())
-//             {
-//                 prev_node = non_sold_models;
-//                 non_sold_models = non_sold_models->Left();
-//                 continue;
-//             }
-//         }
-//         Tree<CarModel> *prev_model;
-//         // Check the sold models.
-//         while (counter != threshhold && sold_models != nullptr)
-//         {
-//             if (sold_models->Left() != nullptr && prev_model != sold_models)
-//             {
-//                 prev_model = sold_models;
-//                 sold_models = sold_models->Left();
-//                 continue;
-//             }
-//             // Add the current.
-//             CarModel *current = sold_models->Data();
-//             type_array[counter] = current->Type();
-//             model_array[counter] = current->Id();
-//             counter++;
-//             if (sold_models->Right() != nullptr && prev_model != sold_models)
-//             {
-//                 prev_model = sold_models;
-//                 sold_models = sold_models->Right();
-//                 continue;
-//             }
-//             prev_model = sold_models;
-//             sold_models = sold_models->Parent();
-//         }
