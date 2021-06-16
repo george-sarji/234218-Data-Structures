a2108a5aa8499068cddaf2ac7f34c2535b320739
christine shaheen <christeensha@campus.technion.ac.il>
2021-06-16 20:17:42 +0000

    Add new file

---
+++ Assignments/wet 2/CarSale.h
@@ -0,0 +1,33 @@
+#ifndef CARSALE_H
+#define CARSALE_H
+
+
+namespace structures
+{
+    class CarSale
+    {
+        private:
+            int typeId, sales;
+
+        public:
+            CarSale(int typeID, int sales) : typeId(typeId), sales(sales) {}
+            CarSale(const CarSale& carType) = default;
+            ~CarSale() = default;
+            CarSale& operator=(const CarSale& carType) = default;
+
+            bool operator<(const CarSale& carType) const;
+            bool operator<=(const CarSale& carType) const;
+            bool operator>(const CarSale& carType) const;
+            bool operator>=(const CarSale& carType) const;
+            bool operator==(const CarSale& carType) const;
+
+            int getTypeID() const;
+            int getSales() const;
+
+            void setTypeID(int newID);
+            void setSales(int sales);
+    };
+}
+
+
+#endif //CARSALE_H
