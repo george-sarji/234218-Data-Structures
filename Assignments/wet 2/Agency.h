29e2d556c5cc42e6150c99964a952d18c1f1b461
george-sarji <georgesarji@gmail.com>
2021-06-18 18:58:59 +0300

    Added memory allocation catches

--- Assignments/wet 2/Agency.h
+++ Assignments/wet 2/Agency.h
@@ -5,6 +5,7 @@
 #include "Tree.h"
 #include "RankTree.h"
 #include "CarSale.h"
+#include "Exception.h"
 
 namespace structures
 {
@@ -14,11 +15,31 @@ namespace structures
         int agency_id;
         int total_types;
         int total_sales;
-        Tree<CarType>* car_types;
-        RankTree<CarSale>* car_sales;
+        Tree<CarType> *car_types;
+        RankTree<CarSale> *car_sales;
+
     public:
         Agency() = default;
-        Agency(const int i) : agency_id(i), total_types(0), total_sales(0), car_types(new Tree<CarType>()), car_sales(new RankTree<CarSale>()) {}
+        Agency(const int i) : agency_id(i), total_types(0), total_sales(0)
+        {
+            try
+            {
+                this->car_types = new Tree<CarType>();
+            }
+            catch (const std::bad_alloc &e)
+            {
+                throw MemoryError();
+            }
+            try
+            {
+                this->car_sales = new RankTree<CarSale>();
+            }
+            catch (const std::bad_alloc &e)
+            {
+                delete this->car_types;
+                throw MemoryError();
+            }
+        }
         ~Agency();
         Agency(const Agency &);
 
@@ -26,13 +47,13 @@ namespace structures
         int getAgencyId() const;
         int getTotalTypes() const;
         int getTotalSales() const;
-        Tree<CarType>* getCarTypes() const;
-        RankTree<CarSale>* getCarSales() const;
+        Tree<CarType> *getCarTypes() const;
+        RankTree<CarSale> *getCarSales() const;
         void setAgencyId(int);
         void setTotalTypes(int);
         void setTotalSales(int);
-        void setCarTypes(Tree<CarType>* carType);
-        void setCarSales(RankTree<CarSale>* carSale);
+        void setCarTypes(Tree<CarType> *carType);
+        void setCarSales(RankTree<CarSale> *carSale);
 
         // * Operator overloads
         bool operator<(const Agency &);
@@ -44,7 +65,7 @@ namespace structures
         // * Utility functions
         void updateAgency(const Agency &);
 
-        CarType** getArrayFromNormalTree() const;
+        CarType **getArrayFromNormalTree() const;
         int getTreeSize() const;
     };
 }
